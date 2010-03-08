8f3d91c6bed4a511e006c4fd99db099966bcce68
Luis Perichon <luigi@luigi-laptop.(none)>
2010-03-08 19:11:25 -0300

    formtastic blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,14 @@
 class PostsController < SimpleBlog::PostsController
-  
+  before_filter :navigation
+
+  def navigation
+    @posts ||= Post.all
+    @post ||= params[:id] ? Post.find(params[:id]) : @posts.last
+    unless @post.blank?
+      @previous_post = Post.previous(@post).first
+      @next_post = Post.next(@post).first
+    end
+
+    @post ||= Post.new
+  end
 end
--- vendor/plugins/simple_blog/app/models/post.rb
+++ vendor/plugins/simple_blog/app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  named_scope :previous, lambda { |p| {:conditions => ["id < ?", p.id], :limit => 1, :order => "id DESC"} }
+  named_scope :next, lambda { |p| {:conditions => ["id > ?", p.id], :limit => 1, :order => "id"} }
 end
