70a9352544d2e6c48fde78d2a6e07330ba6fa1a6
Luis Perichon <luigi@luigi-laptop.(none)>
2010-03-10 15:14:06 -0300

    fix for rails 2.3.2

--- config/initializers/new_rails_defaults.rb
+++ config/initializers/new_rails_defaults.rb
@@ -11,7 +11,8 @@ if defined?(ActiveRecord)
   ActiveRecord::Base.store_full_sti_class = true
 end
 
-ActionController::Routing.generate_best_match = false
+#fix for rails 2.3.2
+#ActionController::Routing.generate_best_match = false
 
 # Use ISO 8601 format for JSON serialized times and dates.
 ActiveSupport.use_standard_json_time_format = true
